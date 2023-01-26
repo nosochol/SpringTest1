05263686e291ea20d831ac1a00cf45eb0df7ced9
nosochol <nosochol@naver.com>
2023-01-26 22:26:13 +0900

    sample test

--- SpringTest11/src/main/java/com/springapp/mvc/HelloController.java
+++ SpringTest11/src/main/java/com/springapp/mvc/HelloController.java
@@ -11,6 +11,7 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
+		model.addAttribute("message22", "Hello world!222");
 		return "hello";
 	}
 }
